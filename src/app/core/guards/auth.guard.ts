bb47f2fefba50c91522e91a4f69841c5b3b22c54
frank9615 <accardifrancesco@hotmail.it>
2022-04-29 12:49:29 +0200

    Fix AuthGuard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -11,16 +11,19 @@ export class AuthGuard implements CanActivate {
 
   constructor(
     private authService: AuthService,
-    private route: Router
+    private router: Router
 
   ) { }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     const user: User = this.authService.currentUserValue;
-    // da implementare in base al ruolo
-    return true;
+    if (user) {
+      if (route.data['roles'] && route.data['roles'].indexOf(user.role) !== -1) {
+        return true;
+      }
+    }
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+    return false;
   }
-
-
 }
